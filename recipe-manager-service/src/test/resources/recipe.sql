c4af57e5cb4edddd0220f204e793c53d36367e28
saruljothi <saruljothi@mobiquityinc.com>
2022-05-19 11:08:03 +0200

    refactors the code

--- recipe-manager-service/src/test/resources/recipe.sql
+++ recipe-manager-service/src/test/resources/recipe.sql
@@ -6,10 +6,10 @@ insert into ingredient (ingredient_name)
 values ('tomato');
 insert into scale_unit (unit_value)
 values ('kg');
-insert into recipe (recipe_id, cooking_instructions, create_date_time, dish_type, no_of_people_suitable, recipe_name,
+insert into recipe (recipe_id, cooking_instructions, create_date_time, is_vegetarian, no_of_people_suitable, recipe_name,
                     update_date_time, user_id)
 values ('001','cook the food','2022-05-18 15:46:19',false,10,'chicken',null,'emailid@domain.com');
-insert into recipe (recipe_id, cooking_instructions, create_date_time, dish_type, no_of_people_suitable, recipe_name,
+insert into recipe (recipe_id, cooking_instructions, create_date_time, is_vegetarian, no_of_people_suitable, recipe_name,
                     update_date_time, user_id)
 values ('002','cook the food','2022-05-16 15:46:19',false,10,'bread',null,'emailid1@domain.com');
 insert into recipe_ingrediant_quantity (ingredient_name, recipe_id, quantity, unit_value)
